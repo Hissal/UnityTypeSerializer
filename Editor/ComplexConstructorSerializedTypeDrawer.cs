f85885d78f88426b90b52f0f6992772ce04e2d43
Lassi Harjaluoma <hissalofficial@gmail.com>
2026-02-08 22:39:30 +0200

    chore: add metadata files for SerializedType and add missing namespace to ComplexConstructorSerializedTypeDrawer

--- Editor/ComplexConstructorSerializedTypeDrawer.cs
+++ Editor/ComplexConstructorSerializedTypeDrawer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Sirenix.OdinInspector.Editor;
 using Sirenix.Utilities.Editor;
 using UnityEditor;
